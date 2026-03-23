2cf6da06722c1d1a2778b6d59e15d35345c34aeb
Tsuki321 <tsukinokojima123@gmail.com>
2026-03-23 13:51:34 +0800

    feat: Initialize Android application with Gradle setup and Firebase integration.

--- Monitoring, Dashboard, and Control/WaterMonitor/app/build.gradle.kts
+++ Monitoring, Dashboard, and Control/WaterMonitor/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -61,6 +62,8 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
